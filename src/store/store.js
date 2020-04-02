27073c652e41ec1c1af5f558b013190581c440a4
Alex Stefanou <Alex-Stefanou@users.noreply.github.com>
2020-04-02 21:54:30 +0100

    Initialised starting deck

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,10 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
+    coreDeck: [],
     gameRound: 0, // 0 = homescreen, 6 = endscreen
     numPlayers: 4,
     playerNames: [],
-    deck: {artifacts: [], hazards: [], treasure: []}
   },
 
   mutations: {
@@ -19,6 +19,26 @@ export const store = new Vuex.Store({
     resetApp (state) {
       state.gameRound = 0;
       console.log("Application has been reset");
+    },
+    resetDeck (state) {
+      function Treasure(value) {
+        this.type = "treasure",
+        this.value = value
+      };
+      function Hazard(catagory) {
+        this.type = "hazard",
+        this.catagory = catagory
+      };
+      state.deck = [
+        new Treasure(17), new Treasure(15), new Treasure(14), new Treasure(13),
+        new Treasure(11), new Treasure(11), new Treasure(9),  new Treasure(7),  new Treasure(7),  new Treasure(5),  new Treasure(5),
+        new Treasure(4),  new Treasure(3),  new Treasure(2),  new Treasure(1),
+        new Hazard("fire"), new Hazard("fire"), new Hazard("fire"),
+        new Hazard("mummy"), new Hazard("mummy"), new Hazard("mummy"),
+        new Hazard("rocks"), new Hazard("rocks"), new Hazard("rocks"),
+        new Hazard("snake"), new Hazard("snake"), new Hazard("snake"),
+        new Hazard("spider"), new Hazard("spider"), new Hazard("spider")
+      ];
     },
     setArtifactType (state, artifactType) {
       state.artifactType = artifactType;
